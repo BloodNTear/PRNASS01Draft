ec1eba71b894a3a5f49f1e39bf267a97eaee3726
taolaprovip <tranthequanganime@gmail.com>
2022-06-28 19:03:20 +0700

    frmLogin

--- MyStoreWinApp/frmLogin.cs
+++ MyStoreWinApp/frmLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataAccess.MemberRepository;
+using DataAccess;
 
 namespace MyStoreWinApp
 {
@@ -19,8 +21,36 @@ namespace MyStoreWinApp
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
-        {
+        {   
+            
+            try
+            {
+               
+                
+                BaseDAL baseDAL = new BaseDAL();
+                if(txtEmail.Text.Length < 6)
+                    throw new Exception("Sai tên đăng nhập");
+                if(txtPassword.Text.Length < 4)
+                    throw new Exception("Sai mật khẩu");
+                String email = txtEmail.Text.Trim();
+                String password = txtPassword.Text.Trim();
+                var account = new { email, password };
+                var admin = baseDAL.GetConnectionString;
+                MemberRepository memberRepository = new MemberRepository();
+                bool checkLogin = memberRepository.Login(email, password);
 
+                if (checkLogin != false)
+                {
+                    frmMemberList frmMemberList = new frmMemberList();
+                    frmMemberList.ShowDialog();
+                } else
+                {
+                    throw new Exception("Sai tk hoặc mk");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi đăng nhập");
+            }
         }
 
         private void frmLogin_Load(object sender, EventArgs e)
